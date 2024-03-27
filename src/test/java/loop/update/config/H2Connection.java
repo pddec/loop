6a4850640ae671d0cd2c7be145b6c11181bcec3f
pddec <gauslostre@outlook.com>
2024-03-27 13:39:31 -0300

    updated

--- src/test/java/loop/update/config/H2Connection.java
+++ src/test/java/loop/update/config/H2Connection.java
@@ -5,8 +5,6 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 
-@Builder
-@RequiredArgsConstructor
 public class H2Connection {
 
     private final String url;
@@ -17,7 +15,18 @@ public class H2Connection {
     private final Integer maxIdle;
     private final Integer maxOpenPrepStmt;
 
-   
+    public H2Connection(String url, String user, String password,
+                        String className, Integer minIdle,
+                        Integer maxIdle, Integer maxOpenPrepStmt) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+        this.className = className;
+        this.minIdle = minIdle;
+        this.maxIdle = maxIdle;
+        this.maxOpenPrepStmt = maxOpenPrepStmt;
+    }
+
     public BasicDataSource dataSource() {
         BasicDataSource ds = new BasicDataSource();
         if (ds.getDriverClassName() == null || ds.getDriverClassName().equals("")) {
@@ -30,6 +39,61 @@ public class H2Connection {
             ds.setMaxOpenPreparedStatements(maxOpenPrepStmt);
         }
         return ds;
+    }
+
+    public static H2ConnectionBuilder builder(){
+        return new H2ConnectionBuilder();
+    }
+
+    public static class H2ConnectionBuilder{
+        private  String url;
+        private  String user;
+        private  String password;
+        private  String className;
+        private  Integer minIdle;
+        private  Integer maxIdle;
+        private  Integer maxOpenPrepStmt;
+
+        public H2ConnectionBuilder url(String url) {
+            this.url = url;
+            return this;
+        }
+
+        public H2ConnectionBuilder user(String user) {
+            this.user = user;
+            return this;
+        }
+
+        public H2ConnectionBuilder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public H2ConnectionBuilder className(String className) {
+            this.className = className;
+            return this;
+        }
+
+        public H2ConnectionBuilder minIdle(Integer minIdle) {
+            this.minIdle = minIdle;
+            return this;
+        }
+
+        public H2ConnectionBuilder maxIdle(Integer maxIdle) {
+            this.maxIdle = maxIdle;
+            return this;
+        }
+
+        public H2ConnectionBuilder maxOpenPrepStmt(Integer maxOpenPrepStmt) {
+            this.maxOpenPrepStmt = maxOpenPrepStmt;
+            return this;
+        }
+
+        public H2Connection build() {
+            return new H2Connection(this.url, this.user, this.password, this.className,
+                    this.minIdle, this.maxIdle, this.maxOpenPrepStmt);
+        }
+
     }
 
 }
